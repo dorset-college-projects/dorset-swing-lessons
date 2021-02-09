7ca76e1fb12f54fb9f786a56b6e7b3d292178dc6
JOhn Rowley <john@ics.ie>
2021-02-09 11:12:12 +0000

    adding chapter 12

---
+++ LabelTest/src/com/iteachonline/Main.java
@@ -0,0 +1,13 @@
+package com.iteachonline;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(260, 180);
+        labelFrame.setVisible(true);
+    }
+}
---
+++ TextFieldTest/src/com/iteachonline/Main.java
@@ -0,0 +1,13 @@
+package com.iteachonline;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350, 100);
+        textFieldFrame.setVisible(true);
+    }
+}
