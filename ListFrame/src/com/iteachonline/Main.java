92af865a7b4b3cf92f905df9da890afe25b7e740
JOhn Rowley <john@ics.ie>
2021-02-16 12:19:28 +0000

    adding list frame

---
+++ ListFrame/src/com/iteachonline/Main.java
@@ -0,0 +1,13 @@
+package com.iteachonline;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListFrame listFrame = new ListFrame(); // create ListFrame
+        listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        listFrame.setSize(350, 150);
+        listFrame.setVisible(true);
+    }
+}
